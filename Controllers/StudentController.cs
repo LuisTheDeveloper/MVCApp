94ea3a38585a2c124c57a5bf2e0e79bcd2a534d3
Luis.Pedro.Fonseca <Luis.Pedro.Fonseca@outlook.com>
2019-10-13 14:48:35 +0100

    Sunday-01: commiting all the work.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public string Index()
+        {
+            return "This is an Index method of StudentController";
+        }
+    }
+}
